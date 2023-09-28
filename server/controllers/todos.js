e204d5c5f015ba119f48cab726763149bb132dd8
dokimion24 <leecy3610@gmail.com>
2023-09-28 18:23:50 +0900

    feat: mysql connection

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,8 +1,19 @@
+const connection = require('../utils/db');
+
 exports.createTodo = (req, res, next) => {
   console.log(req.body);
 };
 
-exports.getTodos = (req, res, next) => {};
+exports.getTodos = (req, res, next) => {
+  connection.query('SELECT * FROM todolist', (err, data, fields) => {
+    if (err) return next(new AppError(err));
+    res.status(200).json({
+      status: 'success',
+      length: data?.length,
+      data: data,
+    });
+  });
+};
 
 exports.updateTodo = (req, res, next) => {
   console.log(req.body);
---
+++ server/utils/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('MySQL Connected');
+});
+
+module.exports = connection;
