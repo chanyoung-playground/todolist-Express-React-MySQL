0fd2f372212036f3ae38dbdbd46f47bd0934b56b
dokimion24 <leecy3610@gmail.com>
2023-10-01 18:28:20 +0900

    feat: 로그인 기능 구현

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,36 +14,42 @@ exports.signin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const sql = 'SELECT * FROM users WHERE email = ?';
-    const [existedUser] = await db.query(sql, [email]);
+    const [[existedUser]] = await db.query(sql, [email]);
 
-    if (existedUser.length === 0) {
-      return res.status(400).json({ error: 'User not found' });
+    console.log(existedUser);
+
+    if (!existedUser.email) {
+      throw new BadRequestError(400, 'User not found');
     }
 
-    const validPassword = await bcrypt.compare(password, existedUser.password);
-
-    if (!validPassword) {
-      return res.status(400).json({ error: 'Invalid Password' });
+    if (!existedUser.password) {
+      throw new BadRequestError(400, 'Invalid Password');
     }
 
-    // const accessToken = generateAccessToken(
-    //   result.id,
-    //   user.username,
-    //   user.email
-    // );
-    // const refreshToken = generateRefreshToken(
-    //   user.id,
-    //   user.username,
-    //   user.email
-    // );
-    // registerToken(refreshToken, accessToken);
-    // const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
+    const id = await db.query('SELECT LAST_INSERT_ID()');
+
+    const accessToken = generateAccessToken(id, email);
+    const refreshToken = generateRefreshToken(id, email);
+    registerToken(refreshToken, accessToken);
+    const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
     // res.cookie('refreshToken', refreshToken, {
     //   path: '/',
     //   httpOnly: true,
     //   maxAge: 3600 * 24 * 30 * 1000,
     // });
     // return res.send({ content: decoded, accessToken, user });
+
+    res
+      .cookie('refreshToken', refreshToken, {
+        path: '/',
+        httpOnly: true,
+        maxAge: 3600 * 24 * 30 * 1000,
+      })
+      .header('Authorization', accessToken);
+
+    res.status(200).json({
+      status: 'success',
+    });
   } catch (err) {
     next(err);
   }
@@ -63,27 +69,26 @@ exports.signup = async (req, res, next) => {
 
     const createSql = 'INSERT INTO users(email, password) VALUES(?, ?)';
     await db.query(createSql, [email, hasedPassword]);
-    const id = await db.query('SELECT LAST_INSERT_ID()');
+    // const id = await db.query('SELECT LAST_INSERT_ID()');
 
-    // console.log(id);
+    // const accessToken = generateAccessToken(id, email);
+    // const refreshToken = generateRefreshToken(id, email);
 
-    const accessToken = generateAccessToken(id, email);
-    const refreshToken = generateRefreshToken(id, email);
+    // registerToken(refreshToken, accessToken);
 
-    registerToken(refreshToken, accessToken);
+    // const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
 
-    const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
-
-    res
-      .cookie('refreshToken', refreshToken, {
-        path: '/',
-        httpOnly: true,
-        maxAge: 3600 * 24 * 30 * 1000,
-      })
-      .header('Authorization', accessToken);
+    // res
+    //   .cookie('refreshToken', refreshToken, {
+    //     path: '/',
+    //     httpOnly: true,
+    //     maxAge: 3600 * 24 * 30 * 1000,
+    //   })
+    //   .header('Authorization', accessToken);
 
     res.status(200).json({
       status: 'success',
+      message: '회원가입 성공',
     });
   } catch (err) {
     next(err);
