aaa118196ad7319f03feac2543b5884544e64101
dokimion24 <leecy3610@gmail.com>
2023-10-03 21:27:14 +0900

    feat: 인증 미들웨어 추가

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const todosController = require('../controllers/todos');
+const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/', todosController.createTodo);
-router.get('/', todosController.getTodos);
+router.post('/', authMiddleware.authenticate, todosController.createTodo);
+router.get('/', authMiddleware.authenticate, todosController.getTodos);
 router.put('/:id', todosController.updateTodo);
 router.delete('/:id', todosController.deleteTodo);
 
