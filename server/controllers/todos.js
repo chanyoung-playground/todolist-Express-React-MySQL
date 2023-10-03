e2af52a318f5f78d97c51b43e1829388c0c8a757
dokimion24 <leecy3610@gmail.com>
2023-10-03 21:25:24 +0900

    feat: todo 생성 기능

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,12 +1,31 @@
 const db = require('../utils/db');
 
-exports.createTodo = (req, res, next) => {
-  console.log(req.body);
+exports.createTodo = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const { todo } = req.body;
+    console.log(id, todo);
+    const createSql =
+      'INSERT INTO todos (todo, isCompleted ,user_id) VALUES(?, true, ?)';
+    await db.query(createSql, [todo, id]);
+
+    const readSql = `SELECT todos.id as id, todo, isCompleted, user_id FROM todos 
+    JOIN users ON users.id = todos.user_id`;
+    const [result] = await db.query(readSql);
+
+    console.log(result);
+
+    res
+      .status(201)
+      .json({ status: 'success', message: 'todo 생성', data: result });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.getTodos = async (req, res, next) => {
   try {
-    const sql = 'SELECT * FROM todo';
+    const sql = 'SELECT * FROM todos';
     const [result] = await db.query(sql);
     console.log(result);
     res.status(200).json({
